a43c049c26569e28f3fc25526498a604ac713ddf
Yogesh-dsa <yogeshguptag23@gmail.com>
2025-02-25 16:57:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+  const accountId = 144553
+  let accountEmail = "Yogesh@google.com"
+  var accountPassword = "12345"
+  accountCity = "jaipur"
+  let accountState;
+  // accountId = 2 //not allowed
+  accountEmail = "hc@hc.com"
+  accountPassword="21212121"
+  accountCity="Bengaluru"
+
+
+  console.log(accountId);
+  
+
+  console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+  
